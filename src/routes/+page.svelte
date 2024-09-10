dca80c11de7d11d96de3d492a0a9b67a13cb940f
ryan <trongdv@coccoc.com>
2024-09-11 00:19:53 +0700

    fix: scroll list book cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,7 +36,7 @@
 		</div>
 
 		<!-- <h1 class="text-2xl">Books:</h1> -->
-		<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 grid-a my-6">
+		<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 grid-a my-6 pb-6 -mx-6 px-6 h-[calc(100dvh_-_86px)] overflow-x-hidden overflow-y-auto">
 			<a
 				href="/learn/{slugify(
 					'VOCABULARY IN USE Pre-intermediate and Intermediate'.toLowerCase()
